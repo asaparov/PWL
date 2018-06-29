4312403ae995e53c4f5ed79139e1a0451f196ee0
asaparov <abulhair.saparov@gmail.com>
2018-06-28 22:15:50 -0400

    Started implementing the lexer for TPTP-like format for first-order logic formulas.

--- first_order_logic.h
+++ first_order_logic.h
@@ -8,6 +8,15 @@
 #ifndef FIRST_ORDER_LOGIC_H_
 #define FIRST_ORDER_LOGIC_H_
 
+#include <core/lex.h>
+
+using namespace core;
+
+
+/* forward declarations */
+
+struct fol_expression;
+
 enum class fol_term_type {
 	VARIABLE,
 	CONSTANT
@@ -87,5 +96,143 @@ struct fol_expression {
 		fol_exists_expression exists;
 	};
 };
+
+
+/**
+ * Code for tokenizing/lexing first-order logic formulas in TPTP-like format.
+ */
+
+enum class tptp_token_type {
+	LBRACKET,
+	RBRACKET,
+	LPAREN,
+	RPAREN,
+	COMMA,
+	COLON,
+
+	AND,
+	OR,
+	NOT,
+	IF_THEN,
+	IFF,
+	FOR_ALL,
+	EXISTS,
+
+	IDENTIFIER
+};
+
+typedef lexical_token<tptp_token_type> tptp_token;
+
+template<typename Stream>
+inline bool print(tptp_token_type type, Stream& stream) {
+	switch (type) {
+	case tptp_token_type::LBRACKET:
+		return print('[', stream);
+	case tptp_token_type::RBRACKET:
+		return print(']', stream);
+	case tptp_token_type::LPAREN:
+		return print('(', stream);
+	case tptp_token_type::RPAREN:
+		return print(')', stream);
+	case tptp_token_type::COMMA:
+		return print(',', stream);
+	case tptp_token_type::COLON:
+		return print(':', stream);
+	case tptp_token_type::AND:
+		return print('&', stream);
+	case tptp_token_type::OR:
+		return print('|', stream);
+	case tptp_token_type::NOT:
+		return print('~', stream);
+	case tptp_token_type::IF_THEN:
+		return print("=>", stream);
+	case tptp_token_type::IFF:
+		return print("<=>", stream);
+	case tptp_token_type::FOR_ALL:
+		return print('!', stream);
+	case tptp_token_type::EXISTS:
+		return print('?', stream);
+	case tptp_token_type::IDENTIFIER:
+		return print("IDENTIFIER", stream);
+	}
+	fprintf(stderr, "print ERROR: Unknown tptp_token_type.\n");
+	return false;
+}
+
+enum class tptp_lexer_state {
+	DEFAULT,
+	IDENTIFIER,
+}
+
+bool datalog_lex(array<tptp_token>& tokens, FILE* input) {
+	position start = position(1, 1);
+	position current = position(1, 1);
+	tptp_lexer_state state = tptp_lexer_state::DEFAULT;
+	array<char> token = array<char>(1024);
+
+	int prev = 0, next = fgetc(input);
+	bool new_line = false;
+	while (next != -1) {
+		switch (state) {
+		case tptp_lexer_state::IDENTIFIER:
+			if (next == ',' || next == ':' || next == '(' || next == ')'
+			 || next == '[' || next == ']' || next == '&' || next == '|'
+			 || next == '~')
+			{
+				if (!emit_token(tokens, token, start, current, tptp_token_type::IDENTIFIER)
+				 || !tptp_emit_symbol(tokens, current, next))
+					return false;
+				state = tptp_lexer_state::DEFAULT;
+				token.clear();
+			} else if (next == ' ' || next == '\t' || next == '\n' || next == '\r') {
+				if (!emit_token(tokens, token, start, current, tptp_token_type::IDENTIFIER))
+					return false;
+				state = tptp_lexer_state::DEFAULT;
+				token.clear();
+				new_line = (next == '\n');
+			} else {
+				if (!token.add(next)) return false;
+			}
+			break;
+
+		case tptp_lexer_state::DEFAULT:
+			if (next == ',' || next == ':' || next == '(' || next == ')'
+			 || next == '[' || next == ']' || next == '&' || next == '|'
+			 || next == '~')
+			{
+				if (!tptp_emit_symbol(tokens, current, next))
+					return false;
+			} else if (next == '\\') {
+				next = fgetc(input);
+				if (next != '+') {
+					read_error("Expected '+'", current);
+					return false;
+				} if (!emit_token(tokens, current, current + 1, DATALOG_TOKEN_SLASH_PLUS))
+					return false;
+				current.column++;
+			} else if (next == ' ' || next == '\t' || next == '\n' || next == '\r') {
+				new_line = (next == '\n');
+			} else {
+				if (!token.add(next)) return false;
+				state = tptp_lexer_state::IDENTIFIER;
+				start = current;
+			}
+			break;
+		}
+
+		if (new_line) {
+			current.line++;
+			current.column = 1;
+			new_line = false;
+		} else current.column++;
+		prev = next;
+		next = fgetc(input);
+	}
+
+	if (state == tptp_lexer_state::IDENTIFIER) {
+		return emit_token(tokens, token, start, current, tptp_lexer_state::IDENTIFIER);
+	}
+	return true;
+}
 
 #endif /* FIRST_ORDER_LOGIC_H_ */
