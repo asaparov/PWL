1e8752305b2e9634b818171f9fbe3e92588e13eb
asaparov <abulhair.saparov@gmail.com>
2018-08-09 17:43:37 -0400

    Almost done with the deserialization code for in-memory article sources and the fake parser.

--- article.h
+++ article.h
@@ -2,6 +2,7 @@
 #define ARTICLE_H_
 
 #include <core/lex.h>
+#include <cstdint>
 
 using namespace core;
 
@@ -29,6 +30,10 @@ struct article {
 	}
 };
 
+struct in_memory_article_store {
+	hash_map<unsigned int, article>& articles;
+};
+
 
 /**
  * Code for tokenizing/lexing the articles.
@@ -39,7 +44,8 @@ enum class article_token_type {
 	RBRACKET,
 	PERIOD,
 	COLON,
-	IDENTIFIER
+	NEWLINE,
+	TOKEN
 };
 
 typedef lexical_token<article_token_type> article_token;
@@ -47,16 +53,14 @@ typedef lexical_token<article_token_type> article_token;
 template<typename Stream>
 inline bool print(article_token_type type, Stream& stream) {
 	switch (type) {
-	case article_token_type::LBRACKET:
-		return print('[', stream);
-	case article_token_type::RBRACKET:
-		return print(']', stream);
 	case article_token_type::PERIOD:
 		return print('.', stream);
 	case article_token_type::COLON:
 		return print(':', stream);
-	case article_token_type::IDENTIFIER:
-		return print("IDENTIFIER", stream);
+	case article_token_type::TOKEN:
+		return print("TOKEN", stream);
+	case article_token_type::FORMULA:
+		return print("FORMULA", stream);
 	}
 	fprintf(stderr, "print ERROR: Unknown article_token_type.\n");
 	return false;
@@ -64,15 +68,12 @@ inline bool print(article_token_type type, Stream& stream) {
 
 enum class article_lexer_state {
 	DEFAULT,
-	IDENTIFIER,
+	TOKEN,
+	FORMULA
 };
 
 bool article_emit_symbol(array<article_token>& tokens, const position& start, char symbol) {
 	switch (symbol) {
-	case '[':
-		return emit_token(tokens, start, start + 1, article_token_type::LBRACKET);
-	case ']':
-		return emit_token(tokens, start, start + 1, article_token_type::RBRACKET);
 	case '.':
 		return emit_token(tokens, start, start + 1, article_token_type::PERIOD);
 	case ':':
@@ -107,35 +108,56 @@ bool article_lex(array<article_token>& tokens, Stream& input) {
 	bool new_line = false;
 	while (next != WEOF) {
 		switch (state) {
-		case article_lexer_state::IDENTIFIER:
-			if (next == '[' || next == ']' || next == '.' || next == ':')
-			{
-				if (!emit_token(tokens, token, start, current, article_token_type::IDENTIFIER)
+		case article_lexer_state::TOKEN:
+			if (next == '.' || next == ':') {
+				if (!emit_token(tokens, token, start, current, article_token_type::TOKEN)
 				 || !article_emit_symbol(tokens, current, next))
 					return false;
 				state = article_lexer_state::DEFAULT;
 				token.clear(); shift = {0};
+			} else if (next == '{') {
+				if (!emit_token(tokens, token, start, current, article_token_type::TOKEN))
+					return false;
+				state = article_lexer_state::FORMULA;
+				token.clear(); shift = {0};
 			} else if (next == ' ' || next == '\t' || next == '\n' || next == '\r') {
-				if (!emit_token(tokens, token, start, current, article_token_type::IDENTIFIER))
+				if (!emit_token(tokens, token, start, current, article_token_type::TOKEN))
 					return false;
 				state = article_lexer_state::DEFAULT;
 				token.clear(); shift = {0};
 				new_line = (next == '\n');
+				if (new_line && !emit_token(tokens, start, start + 1, article_token_type::NEWLINE))
+					return false;
 			} else {
 				if (!append_to_token(token, next, shift)) return false;
 			}
 			break;
 
-		case article_lexer_state::DEFAULT:
-			if (next == '[' || next == ']' || next == '.' || next == ':')
-			{
-				if (!article_emit_symbol(tokens, current, next))
+		case article_lexer_state::FORMULA:
+			if (next == '}') {
+				if (!emit_token(tokens, token, start, current, article_token_type::FORMULA))
 					return false;
-			} else if (next == ' ' || next == '\t' || next == '\n' || next == '\r') {
-				new_line = (next == '\n');
+				state = article_lexer_state::DEFAULT;
 			} else {
 				if (!append_to_token(token, next, shift)) return false;
-				state = article_lexer_state::IDENTIFIER;
+				new_line = (next == '\n');
+			}
+			break;
+
+		case article_lexer_state::DEFAULT:
+			if (next == '.' || next == ':') {
+				if (!article_emit_symbol(tokens, current, next))
+					return false;
+			} else if (next == '{') {
+				state = article_lexer_state::FORMULA;
+				token.clear(); shift = {0};
+			} else if (next == ' ' || next == '\t' || next == '\n' || next == '\r') {
+				new_line = (next == '\n');
+				if (new_line && !emit_token(tokens, start, start + 1, article_token_type::NEWLINE))
+					return false;
+			} else {
+				if (!append_to_token(token, next, shift)) return false;
+				state = article_lexer_state::TOKEN;
 				start = current;
 			}
 			break;
@@ -149,8 +171,8 @@ bool article_lex(array<article_token>& tokens, Stream& input) {
 		next = fgetwc(input);
 	}
 
-	if (state == article_lexer_state::IDENTIFIER)
-		return emit_token(tokens, token, start, current, article_token_type::IDENTIFIER);
+	if (state == article_lexer_state::TOKEN)
+		return emit_token(tokens, token, start, current, article_token_type::TOKEN);
 	return true;
 }
 
@@ -159,14 +181,110 @@ bool article_lex(array<article_token>& tokens, Stream& input) {
  * Recursive-descent parser for articles.
  */
 
-bool article_interpret(
-	const array<article_token>& tokens, article& out,
-	hash_map<sentence, fol_formula*> logical_forms)
+template<typename TokenType>
+inline bool concatenate(lexical_token<TokenType>* tokens, unsigned int token_count, string& out)
 {
+	unsigned int length = tokens[0].text.length;
+	for (unsigned int i = 1; i < token_count; i++)
+		length += 1 + tokens[i].text.length;
+
+	if (!init(out, length)) return false;
+
 	unsigned int index = 0;
-	while (true) {
-		
+	for (unsigned int i = 0; i < token_count; i++) {
+		if (i > 0) { out[index] = ' '; index++; }
+		for (unsigned int j = 0; j < tokens[i].text.length; j++) {
+			out[index] = tokens[i].text[j];
+			index++;
+		}
 	}
+	return true;
+}
+
+bool article_interpret_sentence(
+		const array<article_token>& tokens,
+		unsigned int& index, sentence& out,
+		hash_map<string, unsigned int>& names)
+{
+	array<token>& tokens = *((array<token>*) alloca(sizeof(array<tokens>)));
+	if (!array_init(tokens, 16)) return false;
+	while (true) {
+		unsigned int token_id;
+		if (!expect_token(tokens, index, article_token_type::TOKEN, "first token in sentence")
+		 || !get_token(tokens[index], token_id, names) || !tokens.add(token_id))
+		{
+			free(tokens); return false;
+		}
+		index++;
+
+		if (index >= tokens.length) {
+			read_error("Expected a token or period at end of sentence", tokens[index].start);
+			free(tokens); return false;
+		} else if (tokens[index].type == article_token_type::PERIOD) {
+			index++; break;
+		} else if (tokens[index].type == article_token_type::TOKEN) {
+			read_error("Expected a token or period at end of sentence", tokens[index].start);
+			free(tokens); return false;
+		}
+	}
+
+	move(tokens.data, out.tokens);
+	move(tokens.length, out.length);
+	return true;
+}
+
+bool article_interpret(
+		const array<article_token>& tokens,
+		unsigned int& index,
+		article& out, unsigned int& article_name,
+		hash_map<sentence, fol_formula*> logical_forms,
+		hash_map<string, unsigned int>& names)
+{
+	unsigned int start = index;
+	if (!expect_token(tokens, index, article_token_type::TOKEN, "article name"))
+		return false;
+	index++;
+	while (tokens[index].type != article_token_type::COLON) {
+		if (!expect_token(tokens, index, article_token_type::TOKEN, "article name"))
+			return false;
+		index+;
+	}
+
+	string name;
+	if (!concatenate(tokens.data + start, index - start, name)) {
+		fprintf(stderr, "article_interpret ERROR: Unable to construct string for article name.\n");
+		return false;
+	} else if (!get_token(name, article_name, names)) {
+		free(name);
+		return false;
+	}
+	free(name);
+
+	array<sentence>& sentences = *((array<sentence>*) alloca(sizeof(array<sentence>)));
+	if (!array_init(sentences, 8))
+		return false;
+	while (true) {
+		if (!sentences.ensure_capacity(sentences.length + 1)
+		 || !article_interpret_sentence(tokens, index, sentences[sentences.length], names))
+		{
+			for (sentence& s : sentences) free(s);
+			free(sentences); return false;
+		}
+		sentences.length++;
+
+		if (index >= tokens.length) {
+			break;
+		} else if (tokens[index].type == article_token_type::NEWLINE) {
+			index++; break;
+		} else if (tokens[index].type == article_token_type::FORMULA) {
+			/* TODO: parse fol_formula here */
+			index++;
+		}
+	}
+
+	move(sentences.data, article.sentences);
+	move(sentences.length, article.sentence_count);
+	return true;
 }
 
 #endif /* ARTICLE_H_ */
